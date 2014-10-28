c3c4299607f63c075626abfb425d378d8c09c905
Ben Sammons <bsammnz@gmail.com>
2014-10-28 11:53:57 -0700

    I do have an eigenface.....

--- face.cpp
+++ face.cpp
@@ -5,6 +5,21 @@
 
 namespace fs = boost::filesystem;
 
+cv::Mat norm_0_255(cv::Mat _src) {
+    cv::Mat src = _src;
+    // Create and return normalized image:
+    cv::Mat dst;
+    switch(src.channels()) {
+    case 1:
+        cv::normalize(_src, dst, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+        break;
+    default:
+        src.copyTo(dst);
+        break;
+    }
+    return dst;
+}
+
 void face::serializeAndWriteEigenvectorsToFile( std::string filepath ) 
 {
 
@@ -18,6 +33,75 @@ void face::readEigenvectorsFromFile( std::string filepath )
 std::vector< std::string > face::readFileNamesFromDirectory( std::string dirpath )
 {
 
+}
+void dumpMatProps(cv::Mat in) {
+  std::cout << "type: " << in.type() << std::endl;
+  std::cout << "dims: " << in.size() << std::endl;
+}
+/* assumes input is nx1 and of type float */
+void copy1DMatInto2DFloat( cv::Mat& src, cv::Mat& dest, int rows, int cols ) {
+  dest.create(rows, cols, src.type());
+  for (int i = 0; i < src.rows; i++) {
+    int row = i / cols;
+    int col = i % cols;
+    dest.at<float>(col, row) = src.at<float>(0, i);
+  }
+
+}
+/* make sure the face is grey, dot products
+each eigenvector against the image */
+cv::Mat face::applyToImage( cv::Mat inputImage ) 
+{
+
+  //std::cout << "cols: " << this->eigenvectors.cols << std::endl;
+  cv::Mat scaledInputImage;
+  cv::Mat scaledInputImageAsFloats;
+  cv::Mat results;
+  results.create(this->eigenvectors.cols, 1, CV_64FC1 );/* nx1 vector */
+  /* scale the input image to the eigenvector dims */
+  cv::resize( inputImage, scaledInputImage, this->enforcedSize );
+  
+  for (int i = 0; i < this->eigenvectors.cols; ++i) {
+
+    cv::Mat ev = this->eigenvectors.col(i).clone();
+    // Reshape to original size & normalize to [0...255] for imshow.
+    cv::Mat grayscale = norm_0_255(ev.reshape(1, this->enforcedSize.height));
+    // Show the image & apply a Jet colormap for better sensing.
+    cv::Mat cgrayscale;
+    cv::applyColorMap(grayscale, cgrayscale, cv::COLORMAP_JET);
+    // Display or save:
+    imshow(cv::format("eigenface_%d", i), cgrayscale);
+    // cv::Mat curEigenVect1d = this->eigenvectors.col(i);
+    // cv::Mat curEigenVect1dAsInts;
+    // cv::Mat curEigenVect;
+    // scaledInputImage = scaledInputImage.reshape(0, 1);
+    // scaledInputImage.convertTo( scaledInputImageAsFloats, CV_64FC1 );
+
+    // // dumpMatProps( scaledInputImageAsFloats);
+    // // dumpMatProps( this->sampleMean);
+    // scaledInputImageAsFloats -= this->sampleMean;
+    // curEigenVect1dAsInts = norm_0_255(curEigenVect1d);
+    // curEigenVect1dAsInts.convertTo( curEigenVect1d, CV_64FC1 );
+
+    // // /* make sure the mats are the same dimensions */
+    // // copy1DMatInto2DFloat( 
+    // //  curEigenVect1d,
+    // //  curEigenVect,
+    // //  this->enforcedSize.width,
+    // //  this->enforcedSize.height );
+    // // std::cout << std::endl << scaledInputImageAsFloats.type() << " " << scaledInputImageAsFloats.size();
+    // // std::cout << std::endl << curEigenVect1d.type() << " " << curEigenVect1d.size();
+    // results.at<float>( 1, i ) = ( scaledInputImageAsFloats.dot( curEigenVect1d.t() ) );
+  }
+
+  //results = norm_0_255(results);
+  cv::Mat normalizedResult;
+  //results.convertTo( normalizedResult, CV_64FC1 );
+  // std::cout << "type:" << scaledInputImageAsFloats.type() << " " << curEigenVect.type() << std::endl;
+  // std::cout << "size:" << scaledInputImageAsFloats.size() << " " << curEigenVect.size() << std::endl; 
+  // std::cout << "dot:" << scaledInputImageAsFloats.dot( curEigenVect ) << std::endl;
+  cv::waitKey(0);
+  return normalizedResult;
 }
 
 /* note - assumes greyscale, will resize to match so
@@ -37,7 +121,7 @@ void face::constructEigenFaceFromGreyFaceImagesInDir(
   /* iterate over the files at the first level in the folder */
   fs::directory_iterator dirEndIter;/* I guess these are initialized to the end */
   for ( fs::directory_iterator directoryIter( collectionFullPath );
-        directoryIter != dirEndIter;
+        directoryIter != dirEndIter && inputImages.size() < 400;
         ++directoryIter )
   {
     if ( fs::is_regular_file( directoryIter->status() ) ) 
@@ -71,16 +155,11 @@ void face::constructEigenFaceFromGreyFaceImagesInDir(
 
 
   /* pull stats out of the model */
-  cv::Mat eigenvalues  = model->getMat( "eigenvalues"  );
-  cv::Mat eigenvectors = model->getMat( "eigenvectors" );
- // std::cout << std::endl << eigenvalues << std::endl;
-  cv::Mat sampleMean   = model->getMat( "mean"         );
-  //std::cout << sampleMean;
-  std::cout << eigenvalues;
-  /* write the eigenvectors to a file */
-
-  //this->serializeAndWriteEigenvectorsToFile( outPutFilePathForEigenvectors );
-
+  this->eigenvalues  = model->getMat( "eigenvalues"  ).clone();
+  this->eigenvectors = model->getMat( "eigenvectors" ).clone();
+  this->sampleMean   = model->getMat( "mean" ).clone();
+  this->eigenvectorsComputed = true;
+  // std::cout << this->eigenvalues;
 }
 
 
@@ -88,5 +167,20 @@ int main(int argc, char const *argv[])
 {
   face eigenface = face();
   eigenface.constructEigenFaceFromGreyFaceImagesInDir("./croppedfaces");
+  
+  /* note that it is important that the images are greyscale, and
+  are the same size as the samples */
+
+  /* this will test to see if two similar images get
+  a low euclidian distance */
+  cv::Mat weightsIm1 = eigenface.applyToImage( cv::imread("./croppedfaces/yaleB01_P00A+070E+45.pgm",0) );
+  cv::Mat weightsIm2 = eigenface.applyToImage( cv::imread("./croppedfaces/yaleB01_P00A+110E+40.pgm",0) );
+  std::cout << weightsIm1 << std::endl << weightsIm2;
+  std::cout << "norm is :" << cv::norm( weightsIm1, weightsIm2 ) << " for pictures of the same guy " << std::endl;
+  cv::Mat weightsIm3 = eigenface.applyToImage( cv::imread("./croppedfaces/yaleB01_P00A+070E+45.pgm",0) );
+  cv::Mat weightsIm4 = eigenface.applyToImage( cv::imread("./croppedfaces/yaleB02_P00A+110E+15.pgm",0) );
+  
+  std::cout << "norm is :" << cv::norm( weightsIm3, weightsIm4 ) << " for pictures of diff people " << std::endl;
+
   return 0;
 }
--- face.hpp
+++ face.hpp
@@ -8,17 +8,24 @@ void serializeAndWriteEigenvectorsToFile( std::string filepath );
 void readEigenvectorsFromFile( std::string filepath );
 void constructEigenFaceFromGreyFaceImagesInDir( 
   std::string collectionDir );
+cv::Mat applyToImage( cv::Mat );
 
 private:
+
+bool eigenvectorsComputed = false;
+cv::Mat eigenvectors;
+cv::Mat eigenvalues;
+cv::Mat sampleMean;
 
 /* all images this object handles are forced to be
 this scale */
 static const cv::Size enforcedSize;
 static const int numberOfComponentsToUse;
+
 std::vector< std::string > readFileNamesFromDirectory( std::string dirpath );
 
 };
 
 /* static initializers */
 const cv::Size face::enforcedSize = cv::Size(36,36);
-const int face::numberOfComponentsToUse = 10;
+const int face::numberOfComponentsToUse = 5;
